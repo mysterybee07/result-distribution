a8005ec92db4d5f042bdb605029b4d4dce52572e
mysterybee07 <biraj2019pudasaini@gmail.com>
2024-06-02 21:39:07 +0545

    edit and update for batch done

--- controllers/batchController.go
+++ controllers/batchController.go
@@ -52,3 +52,44 @@ func CreateBatch(c *fiber.Ctx) error {
 	// Return a success response with the created batch
 	return c.Redirect("/batches")
 }
+
+func EditBatch(c *fiber.Ctx) error {
+	err := c.Render("/batch/editbatch", fiber.Map{})
+	if err != nil {
+		c.Status(fiber.StatusInternalServerError).SendString("Error rendering page")
+		return err
+	}
+	return nil
+}
+
+func UpdateBatch(c *fiber.Ctx) error {
+	id := c.Params("id")
+
+	var batch models.Batch
+	if err := initializers.DB.First(&batch, id).Error; err != nil {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"error": "Batch not found",
+		})
+	}
+	if err := c.BodyParser(&batch); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "Cannot parse JSON",
+		})
+	}
+	// Check if a batch with the same year already exists
+	var existingBatch models.Batch
+	if err := initializers.DB.Where("year = ?", batch.Year).First(&existingBatch).Error; err == nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "Batch already exists",
+		})
+	}
+	if err := initializers.DB.Save(&batch).Error; err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "Could not update batch",
+		})
+	}
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"message": "Batch updated successfully",
+		"batch":   batch,
+	})
+}
