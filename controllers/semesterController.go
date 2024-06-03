e52e626ce64e1dbb8bdcd33defc2cdd39ef30517
mysterybee07 <biraj2019pudasaini@gmail.com>
2024-06-03 20:52:10 +0545

    working on updating semester

--- controllers/semesterController.go
+++ controllers/semesterController.go
@@ -64,3 +64,52 @@ func StoreSemester(c *fiber.Ctx) error {
 
 	return c.Redirect("/semesters")
 }
+
+func EditSemester(c *fiber.Ctx) error {
+	err := c.Render("dashboard/semester/editSemester", fiber.Map{})
+	if err != nil {
+		c.Status(fiber.StatusInternalServerError).SendString("Error rendering page")
+		return err
+	}
+	return nil
+}
+
+func UpdateSemester(c *fiber.Ctx) error {
+	id := c.Params("id")
+
+	var semester models.Semester
+
+	if err := initializers.DB.Find(&semester, id).Error; err != nil {
+		return c.Status(fiber.StatusInternalServerError).SendString("Semester not found")
+	}
+
+	if err := c.BodyParser(&semester); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "Cannot parse JSON",
+		})
+	}
+	var program models.Program
+	if err := initializers.DB.First(&program, semester.ProgramID).Error; err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "Program not found",
+		})
+	}
+
+	var existingSemester models.Semester
+	// Check if a semester with the same name exists within the same program
+	if err := initializers.DB.Where("name = ? AND program_id = ?", semester.Name, semester.ProgramID).First(&existingSemester).Error; err == nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "Semester already exists for this program",
+		})
+	}
+
+	if err := initializers.DB.Save(&semester).Error; err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "Could not update semester",
+		})
+	}
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"message":  "Semester updated successfully",
+		"semester": semester,
+	})
+}
