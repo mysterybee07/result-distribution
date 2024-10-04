13fcde474f8a597d21d28619c417a7e1b3124de5
Hachimankira <kiran9860819025@gmail.com>
2024-10-04 23:20:08 +0545

    frontend started

---
+++ frontend/src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Homepage from './pages/Homepage'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Exam from './pages/Exam'
+import Profile from './pages/Profile'
+import Result from './pages/Result'
+
+function App() {
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/exam" element={<Exam />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/result" element={<Result />} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
---
+++ frontend/src/pages/Profile.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Layout from '../layout/Layout'
+import { Button } from '../components/ui/button'
+
+const Profile = () => {
+    return (
+        <Layout>
+            <div className='p-4 bg-blue-500 text-white'>Profile</div>
+            <Button variant="secondary">Click me</Button>
+
+        </Layout>
+    )
+}
+
+export default Profile
